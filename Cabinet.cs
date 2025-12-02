18099326bb9e3fb1fba377cb9b2d8bb3187e7d2f
nguyenmh <minh-huy.nguyen@etu.univ-grenoble-alpes.fr>
2025-12-02 14:58:29 +0100

    Finir parti 7.3.1, changer dans Cabinet.cs et Program.cs

--- Cabinet.cs
+++ Cabinet.cs
@@ -25,17 +25,88 @@ public class Cabinet
           {
               switch (reader.NodeType)
               {
-                  case XmlNodeType.XmlDeclaration:
-                      // instructions à executer quand le prompt est détecté
-                      Console.Write("Found XML declaration (<?xml version='1.0' encoding='utf-8'?> )");
-                      break;
-                  
                   case XmlNodeType.Document:
                       // instructions à executer quand on entre dans le document
                       Console.Write("Entering the document");
                       break;
+                  
+                  case XmlNodeType.Element:
+                      // instructions à executer quand on entre dans un élement;
+                      // Ce message doit afficher le nom de l'élement 
+                      Console.WriteLine("Starts the element {0}", reader.Name);
+                      // et le nombre d'attributs qu'il contient 
+                      Console.WriteLine("Nombre d'attributs {0}", reader.AttributeCount);
+                      break;
+                  
+                  case XmlNodeType.EndElement:
+                      // instructions à executer quand on sort d'un élément
+                      Console.WriteLine("Ends the element {0}", reader.Name);
+                      break;
+                  
+                  case XmlNodeType.Text:
+                      // instructions à executer quand on trouve du texte
+                      Console.WriteLine("Text node value = {0}", reader.GetValueAsync());
+                      break;
+                  
+                  case XmlNodeType.Attribute:
+                      // instructions à executer quand on trouve un attribut
+                      Console.WriteLine("Attribute {0}", reader.Name);
+                      // contenu de l'attribut
+                      Console.WriteLine("Attribute node value = {0}", reader.ReadElementContentAsString());
+                      break;
+                  
+                  default:
+                      // instructions à executer sinon
+                      Console.WriteLine("Other node of type {0} with value {1}", reader.NodeType, reader.Value);
+                      break;
               }
           }
       }
+  }
+  
+  // Ecrivez une autre fonction qui permet de récupérer le texte d’éléments particuliers (par exemple tous les noms, ou tous les noms des infirmiers).
+  // Idéalement, votre recherche doit être passée en paramètre de la fonction.
+  static public HashSet<String> GetNom(String filename)
+  {
+      XmlReader reader =  XmlReader.Create(filename);
+      HashSet<string> noms = new HashSet<string>();
+      while (reader.Read())
+          switch (reader.NodeType)
+          {
+              case XmlNodeType.Element:
+                  if (reader.Name == "cabinet")
+                      Console.WriteLine("Commence à explorer la cabinet");
+                  
+                  if (reader.IsStartElement("nom"))
+                  {
+                      reader.MoveToFirstAttribute();
+                      Console.WriteLine("   -> un attribut {0} trouvé, de valeur {1}", reader.Name, reader.Value);
+                      noms.Add(reader.Name);
+                  }
+                  break;
+              case XmlNodeType.EndElement:
+                  if (reader.Name == "cabinet")
+                      Console.WriteLine("Ending the element {0}", reader.Name);
+                  break;
+          }
+      return noms;
+  }
+  // Créez une autre fonction utilisant le XmlReader pour compter combien d’actes différents devront être
+  // effectués, tous patients confondus.
+  static public int CountActes(String filename)
+  {
+      XmlReader reader = XmlReader.Create(filename);
+      int actes = 0;
+      List<String> ids = new List<String>();
+      reader.MoveToContent();
+      while (reader.Read())
+          switch (reader.NodeType)
+          {
+              case XmlNodeType.Element:
+                  if (reader.IsStartElement("acte"))
+                    actes++;
+                  break;
+          }
+      return actes;
   }
 }
--- Program.cs
+++ Program.cs
@@ -2,4 +2,14 @@
 
 using CabinetInfirmier;
 
+
 XMLUtils.ValidateXmlFileAsync("http://www.univ-grenoble-alpes.fr/l3miage/medical", "./data/xml/cabinet.xml", "./data/xsd/cabinet.xsd");
+
+Console.WriteLine("Nombre de actes comptés :" + Cabinet.CountActes("./xml/cabinet.xml"));
+HashSet<string> nominfirs = Cabinet.GetNom("./xml/cabinet.xml");
+
+Console.WriteLine("Liste des noms des infirmiers récupérés :");
+foreach (var nom in nominfirs)
+{
+    Console.WriteLine(" -> " + nom);
+}
